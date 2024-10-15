e76c395a06bea91b817bc6bf782d5d0aac44b914
Hans Åkerman <113355864+heinzfieldmann@users.noreply.github.com>
2024-10-15 15:19:38 +0200

    update

--- test/activation_test.py
+++ test/activation_test.py
@@ -1,25 +1,21 @@
 import numpy as np
 
+
+v = [1.0,-10.0,0.0,15.0,-2.0]
+
+
 def sig(x):
     return 1/(1+np.exp(-x))
 
-x = 1.0
-print('Applying Sigmoid Activation on (%.1f) gives %.1f' % (x, sig(x)))
-print(np.exp(-x))
 
-x = -10.0
-print('Applying Sigmoid Activation on (%.1f) gives %.1f' % (x, sig(x)))
-print(np.exp(-x))
+def relu(x):
+#the relu truncates all values less than 0
+    return max(0,x)
 
-x = 0.0
-print('Applying Sigmoid Activation on (%.1f) gives %.1f' % (x, sig(x)))
-print(np.exp(-x))
+for x in v:
+    print('Applying SIGMOID Activation on (%.1f) gives %.1f' % (x, sig(x)))
+    print('Applying RELU Activation on (%.1f) gives %.1f' % (x, relu(x)))
 
-x = 15.0
-print('Applying Sigmoid Activation on (%.1f) gives %.1f' % (x, sig(x)))
-print(np.exp(-x))
 
-x = -2.0
-print('Applying Sigmoid Activation on (%.1f) gives %.1f' % (x, sig(x)))
-print(np.exp(-x))
+
 
